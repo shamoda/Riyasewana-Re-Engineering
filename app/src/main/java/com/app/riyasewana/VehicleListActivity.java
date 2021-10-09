302d49a2699803a4a7c3bccdb4cb0c17c2e3fda2
senath999 <senathweerasingha@gmail.com>
2021-10-09 09:38:31 +0530

    commit option list

--- app/src/main/java/com/app/riyasewana/VehicleListActivity.java
+++ app/src/main/java/com/app/riyasewana/VehicleListActivity.java
@@ -2,10 +2,12 @@ package com.app.riyasewana;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,20 +23,25 @@ import com.app.riyasewana.viewholder.UserDetailsViewHolder;
 import com.app.riyasewana.viewholder.VehicleListViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
+
 public class VehicleListActivity extends AppCompatActivity {
 
     private TextView closeBtn;
     private SearchView searchSpare;
+    private ChipGroup chipGroup;
 
     private DatabaseReference vehicleRef;
     private RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
-
+    String[] types = {"SUV","Jeep","BMW","Auto","Lorry","Allion","Hybrid","Pulsar","Hutch back"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,13 +50,25 @@ public class VehicleListActivity extends AppCompatActivity {
 
         closeBtn = findViewById(R.id.ss_vehicles_list_close_btn);
         searchSpare = findViewById(R.id.ss_vehicle_list_search_view);
-
+        chipGroup = findViewById(R.id.ss_options);
         vehicleRef = FirebaseDatabase.getInstance().getReference().child("Vehicle");
 
         recyclerView = findViewById(R.id.ss_vehicle_list_view);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+        for(int i=0; i < types.length ; i++){
+            Chip chip = new Chip(this);
+            ChipDrawable drawable =  ChipDrawable.createFromAttributes(this,null,0,R.style.Widget_MaterialComponents_Chip_Choice);
+
+            chip.setChipDrawable(drawable);
+            chip.setPadding(10,10,10,10);
+            chip.setChipBackgroundColor(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.colorPrimaryDark)));
+            chip.setTextColor(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.CardColor)));
+            chip.setText(types[i]);
+            chipGroup.addView(chip);
+        }
 
         searchSpare.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -89,12 +108,12 @@ public class VehicleListActivity extends AppCompatActivity {
         FirebaseRecyclerAdapter<Vehicle, VehicleListViewHolder> adapter = new FirebaseRecyclerAdapter<Vehicle, VehicleListViewHolder>(options) {
             @Override
             protected void onBindViewHolder(@NonNull VehicleListViewHolder vehicleListViewHolder, int i, @NonNull final Vehicle vehicle) {
-               vehicleListViewHolder.specialization.setText("Rs. " + vehicle.getPrice());
-               vehicleListViewHolder.name.setText(vehicle.getName());
+                vehicleListViewHolder.specialization.setText("Rs. " + vehicle.getPrice());
+                vehicleListViewHolder.name.setText(vehicle.getName());
 //                doctorDetailsViewHolder.date.setText(spare.getD);
                 Picasso.get().load(vehicle.getImg1()).into(vehicleListViewHolder.image);
 
-               vehicleListViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                vehicleListViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         Intent intent = new Intent(VehicleListActivity.this, VehicleDetailedView.class);
@@ -155,4 +174,3 @@ public class VehicleListActivity extends AppCompatActivity {
         adapter.startListening();
     }
 }
-
